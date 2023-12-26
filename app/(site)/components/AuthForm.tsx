c2088363877a418ff9a4c260a238957f2e8192cf
moraiskaua <moraiskaua03@gmail.com>
2023-12-26 10:40:23 -0300

    feat: create button component

---
+++ app/(site)/components/AuthForm.tsx
@@ -0,0 +1,82 @@
+'use client';
+
+import Button from '@/app/components/Button';
+import Input from '@/app/components/inputs/Input';
+import { useCallback, useState } from 'react';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+
+type VariantType = 'LOGIN' | 'REGISTER';
+
+const AuthForm = () => {
+  const [variant, setVariant] = useState<VariantType>('LOGIN');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const toggleVariant = useCallback(() => {
+    if (variant === 'LOGIN') {
+      setVariant('REGISTER');
+    } else {
+      setVariant('LOGIN');
+    }
+  }, [variant]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = data => {
+    setIsLoading(true);
+
+    if (variant === 'REGISTER') {
+      // Axios register
+    }
+
+    if (variant === 'LOGIN') {
+      // NextAuth signin
+    }
+  };
+
+  const socialAction = (action: string) => {
+    setIsLoading(true);
+
+    // NextAuth social signin
+  };
+
+  return (
+    <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+      <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
+        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+          {variant === 'REGISTER' && (
+            <Input id="name" label="Name" register={register} errors={errors} />
+          )}
+          <Input
+            id="email"
+            label="Email"
+            type="email"
+            register={register}
+            errors={errors}
+          />
+          <Input
+            id="password"
+            label="Password"
+            type="password"
+            register={register}
+            errors={errors}
+          />
+          <Button disabled={isLoading} type="submit" fullWidth>
+            {variant === 'LOGIN' ? 'Sign In' : 'Register'}
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AuthForm;
--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import AuthForm from './components/AuthForm';
 
 const Home = () => {
   return (
@@ -16,6 +17,7 @@ const Home = () => {
           Sign in to your account
         </h2>
       </div>
+      <AuthForm />
     </div>
   );
 };
