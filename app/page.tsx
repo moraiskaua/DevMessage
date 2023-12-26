b06636622152dc984af65e49cb3e4e78721f4802
moraiskaua <moraiskaua03@gmail.com>
2023-12-26 08:59:22 -0300

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+const Home = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      Hello Messenger!
+    </main>
+  );
+};
+export default Home;
