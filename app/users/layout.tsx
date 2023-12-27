3532e3757c0b0bab636400cd0cf60b1be29b2b85
moraiskaua <moraiskaua03@gmail.com>
2023-12-27 20:26:58 -0300

    feat: create contact list

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,14 +1,21 @@
 import { ReactNode } from 'react';
 import Sidebar from '../components/sidebar/Sidebar';
+import getUsers from '../actions/getUsers';
+import UserList from './components/UserList';
 
 interface UsersLayoutProps {
   children: ReactNode;
 }
 
 const UsersLayout = async ({ children }: UsersLayoutProps) => {
+  const users = await getUsers();
+
   return (
     <Sidebar>
-      <div className="h-full">{children}</div>
+      <div className="h-full">
+        <UserList items={users} />
+        {children}
+      </div>
     </Sidebar>
   );
 };
