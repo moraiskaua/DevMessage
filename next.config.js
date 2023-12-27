ca02d6121cdf9b30ace2ffde0392851fee5cb4ef
moraiskaua <moraiskaua03@gmail.com>
2023-12-27 20:03:48 -0300

    feat: create avatar on sidebar

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   experimental: {
     swcPlugins: [['next-superjson-plugin', {}]],
   },
+  images: {
+    domains: [
+      'res.cloudinary.com',
+      'avatars.githubusercontent.com',
+      'lh3.googleusercontent.com',
+      'cdn.discordapp.com',
+    ],
+  },
 };
 
 module.exports = nextConfig;
